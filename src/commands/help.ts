d222142396c6edaf6000f430f0ff13ece72597b5
v341196137 <38384400+vivian-dai@users.noreply.github.com>
2021-06-22 09:49:57 -0400

    oh always my problem of forgetting to remove debugging

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -8,11 +8,8 @@ export const command: Command = {
     aliases: ["help"],
     description: "the help command",
     execute: async (client: Client, msg: Message, args: Array<any>): Promise<void> => {
-        console.log("got here");
         const embed = new MessageEmbed();
-        msg.channel.send("got here");
         for (const fileName of await fs.readdirSync("./dist/commands").filter(file => file.endsWith(".js"))) {
-            console.log(fileName);
             // name every single command "command" so that it grabs the object named "command" as a Command object
             const command = (await import(`./${fileName}`)).command as Command; // will read one command per file
             embed.addField(command.name, command.description);

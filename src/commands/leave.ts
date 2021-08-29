a7b97eed915ff3d858ecc1b1ecceffdfaa1ca2b8
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2021-08-29 18:57:45 -0400

    small fix

--- src/commands/leave.ts
+++ src/commands/leave.ts
@@ -3,6 +3,7 @@
  */
  import {Client, Message, MessageEmbed} from "discord.js";
  import {Command} from "../commands";
+ import { queue } from "../data/musicQueue";
  export const command: Command = {
      name: "leave",
      aliases: ["leave", "disconnect", "dc"],
@@ -19,6 +20,11 @@
              embed.setColor("#00FF00");
              embed.setTitle("Success!");
              embed.setDescription(`Bye ${msg.guild.me.voice.channel.name}!`);
+             for (const channel of queue) {
+                 if (channel.id === msg.guild.me.voice.channel.id) {
+                     queue.splice(queue.indexOf(channel), 1);
+                 }
+             }
              msg.guild.me.voice.channel.leave();
          }
          msg.channel.send(embed);
